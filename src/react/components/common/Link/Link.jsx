4965ac18b22132ed066f92db45c06ccdffda1e93
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-05 13:58:43 +0200

    Refactor Link component to be bold only when it is a contact link. Added capacity for including extra classes

--- src/react/components/common/Link/Link.jsx
+++ src/react/components/common/Link/Link.jsx
@@ -1,12 +1,18 @@
 import styles from './Link.module.css';
 
-function Link({ children, external, ...props }) {
+function Link({ children, external, isContact, ...props }) {
   const externalAttributes = external
     ? { target: '_blank', rel: 'noreferrer' }
     : {};
 
+  const linkStyle = isContact
+    ? `${styles.link} ${styles['contact-link']}`
+    : styles.link;
+
+  const className = `${props.className ? props.className + ' ' : ''}${linkStyle}`;
+
   return (
-    <a className={styles.link} {...props} {...externalAttributes}>
+    <a {...props} className={className} {...externalAttributes}>
       {children}
     </a>
   );
