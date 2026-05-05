a415071a9bfca2b5c7727aa6f91505c597983bdd
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-05 18:30:31 +0200

    Created React EducationCard component

---
+++ src/react/components/features/EducationCard/EducationCard.jsx
@@ -0,0 +1,44 @@
+import commonStyles from '../../CommonStyles.module.css';
+import styles from './EducationCard.module.css';
+
+import { educationData } from '../../../../data/education';
+
+const mockEducation = educationData.items[0];
+
+function EducationCard({ education = mockEducation, ...props }) {
+  // TODO: Add reveal
+  const className = `${commonStyles.container} ${styles['education-card']}`;
+
+  return (
+    <article {...props} className={className}>
+      <EducationTimeline
+        duration={education.duration}
+        degree={education.degree}
+      />
+      <EducationTitleBlock
+        college={education.college}
+        major={education.major}
+      />
+    </article>
+  );
+}
+
+function EducationTimeline({ duration, degree }) {
+  return (
+    <div className={styles['education-timeline']}>
+      <span className={styles['education-meta']}>{duration}</span>
+      <p className={styles['education-label']}>{degree}</p>
+    </div>
+  );
+}
+
+function EducationTitleBlock({ college, major }) {
+  return (
+    <div className={styles['education-title-block']}>
+      <h3>{college}</h3>
+      <p className={styles['education-major']}>{major}</p>
+    </div>
+  );
+}
+
+export default EducationCard;
