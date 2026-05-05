1f6a822dcbc1425ac96b77168eb76f6669b455bb
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-05 18:08:40 +0200

    Created React ContactCard component

---
+++ src/react/components/features/ContactCard/ContactCard.jsx
@@ -0,0 +1,31 @@
+import styles from './ContactCard.module.css';
+import commonStyles from '../../CommonStyles.module.css';
+import Link from '../../common/Link/Link';
+
+function ContactCard({ summary, links, ...props }) {
+  // TODO add reveal animation
+  const className = `${commonStyles.container} ${styles['contact-card']}`;
+
+  return (
+    <article {...props} className={className}>
+      <p className={commonStyles.paragraph}>{summary}</p>
+
+      <ul
+        className={`${commonStyles['flex-container-row']} ${styles['contact-list']}`}
+      >
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            isContact
+            external={link?.external}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </article>
+  );
+}
+
+export default ContactCard;
--- src/react/components/sections/ContactSection/ContactSection.jsx
+++ src/react/components/sections/ContactSection/ContactSection.jsx
@@ -1,8 +1,8 @@
 import GridAutoCols from '../../common/GridAutoCols/GridAutoCols';
-import ProjectCard from '../../features/ProjectCard/ProjectCard';
 import SectionHeader from '../../common/SectionHeader/SectionHeader';
 import commonStyles from '../../CommonStyles.module.css';
 import { contactData } from '../../../../data/contact';
+import ContactCard from '../../features/ContactCard/ContactCard';
 
 function ContactSection({ ...props }) {
   // TODO add reveal animation
@@ -10,6 +10,7 @@ function ContactSection({ ...props }) {
   return (
     <section {...props}>
       <SectionHeader eyebrow={contactData.eyebrow} title={contactData.title} />
+      <ContactCard summary={contactData.summary} links={contactData.links} />
     </section>
   );
 }
