ce4ad5344e85d1b31f319400193da49fe3c71433
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-06 08:48:41 +0200

    Responsive Contact Section

--- src/react/components/sections/ContactSection/ContactSection.jsx
+++ src/react/components/sections/ContactSection/ContactSection.jsx
@@ -3,21 +3,21 @@ import SectionHeader from '../../common/SectionHeader/SectionHeader';
 import commonStyles from '../../CommonStyles.module.css';
 import { contactData } from '../../../../data/contact';
 import ContactCard from '../../features/ContactCard/ContactCard';
+import styles from './ContactSection.module.scss';
 
 function ContactSection({ ...props }) {
   // TODO add reveal animation
 
   return (
-    <section {...props}>
-      <GridAutoCols min={'0.5fr'} max={'1fr'} fitOrFill="fill">
-        <div>
-          <SectionHeader
-            eyebrow={contactData.eyebrow}
-            title={contactData.title}
-          />
-        </div>
-        <ContactCard summary={contactData.summary} links={contactData.links} />
-      </GridAutoCols>
+    <section className={styles['contact-section']} {...props}>
+      <div>
+        <SectionHeader
+          eyebrow={contactData.eyebrow}
+          title={contactData.title}
+        />
+      </div>
+
+      <ContactCard summary={contactData.summary} links={contactData.links} />
     </section>
   );
 }
