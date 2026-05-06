6433e767e50df2ab58b0f9de08f660de115e824e
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-06 09:58:37 +0200

    Pass props to Reveal component to correctly handle navigation by id

--- src/react/components/common/Reveal/Reveal.jsx
+++ src/react/components/common/Reveal/Reveal.jsx
@@ -2,13 +2,18 @@ import { useReveal } from './useReveal';
 
 import styles from './Reveal.module.css';
 
-export function Reveal({ as: Component = 'div', children, className = '' }) {
+export function Reveal({
+  as: Component = 'div',
+  children,
+  className = '',
+  ...props
+}) {
   const { ref, isVisible } = useReveal();
 
   const combinedClassName = `${styles.reveal} ${isVisible ? styles.revealVisible : ''} ${className}`;
 
   return (
-    <Component ref={ref} className={combinedClassName}>
+    <Component ref={ref} className={combinedClassName} {...props}>
       {children}
     </Component>
   );
