484db4b90ef6af1f04112f954850bbb4c6aff855
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-06 09:37:05 +0200

    Create Reveal component

---
+++ src/react/components/common/Reveal/Reveal.jsx
@@ -0,0 +1,15 @@
+import { useReveal } from './useReveal';
+
+import styles from './Reveal.module.css';
+
+export function Reveal({ as: Component = 'div', children, className = '' }) {
+  const { ref, isVisible } = useReveal();
+
+  const combinedClassName = `${styles.reveal} ${isVisible ? styles.revealVisible : ''} ${className}`;
+
+  return (
+    <Component ref={ref} className={combinedClassName}>
+      {children}
+    </Component>
+  );
+}
