2cf016dcc3add587e2d52c2c608f665dcddb38e9
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-06 07:14:03 +0200

    Created React HeroCopy component

---
+++ src/react/components/features/HeroCopy/HeroCopy.jsx
@@ -0,0 +1,30 @@
+import commonStyles from '../../CommonStyles.module.css';
+import styles from './HeroCopy.module.css';
+
+import PrimaryButton from '../../common/Button/PrimaryButton';
+import SecondaryButton from '../../common/Button/SecondaryButton';
+
+function HeroCopy({ hero = mockHero, ...props }) {
+  return (
+    <div className={styles['hero-copy']} {...props}>
+      <p className={commonStyles.eyebrow}>{hero.eyebrow}</p>
+
+      <h1>{hero.title}</h1>
+
+      <div className={commonStyles['flex-container-row']}>
+        {hero.actions.map((action, index) => {
+          const ButtonVariant =
+            action.variant === 'primary' ? PrimaryButton : SecondaryButton;
+
+          return (
+            <ButtonVariant key={index} href={action.href}>
+              {action.label}
+            </ButtonVariant>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default HeroCopy;
